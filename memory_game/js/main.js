7b5393839e2fcdcd1a7fe6feb877a252a3d8e894
Stef Wong <stef@Stef.fios-router.home>
2020-01-20 18:36:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,29 @@
 // console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+var cards = [
+"queen","queen","king","king"
+];
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push('cardOne');
+
+console.log("User flipped " + cardOne);
+
+let cardTwo = cards[2];
+
+cardsInPlay.push('cardTwo');
+
+console.log("User flipped " + cardTwo);
+
+// if (cardsInPlay.length === 2) {
+
+// }
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+};
