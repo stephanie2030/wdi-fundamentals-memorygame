29682b93aa895d6f7561e64c45029e74bd754b13
Stef Wong <stef@Stef.fios-router.home>
2020-01-20 20:49:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,45 @@
 // console.log("Up and running!");
 
-var cards = [
-"queen","queen","king","king"
-];
+const cards = [
+// "queen","queen","king","king"
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage:'images/queen-of-hearts.png',
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage:'images/queen-of-diamonds.png',
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage:'images/king-of-hearts.png',
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage:'images/king-of-diamonds.png',
+}
+	];
 
 cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  	console.log("You found a match!");
+  	alert("You found a match!");
 	} else {
-  	console.log("Sorry, try again.");
+  	alert("Sorry, try again.");
 	}
 }
 
 function flipCard(cardId) {
 var cardId = cardId;
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
 checkForMatch();
 // if (cardsInPlay[0] === cardsInPlay[1]) {
