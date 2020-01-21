20cb6a3512a8c84dbae646d3de10a7ceb759b5ab
Stef Wong <stef@Stef.fios-router.home>
2020-01-20 23:28:21 -0500

    created game logic DOM & Events

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,30 +1,27 @@
 // console.log("Up and running!");
 
-const cards = [
-// "queen","queen","king","king"
-{
-rank: 'queen',
-suit: 'hearts',
-cardImage:'images/queen-of-hearts.png',
+var cards = [ {
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage:"images/queen-of-hearts.png",
 },
 {
-rank: 'queen',
-suit: 'diamonds',
-cardImage:'images/queen-of-diamonds.png',
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage:"images/queen-of-diamonds.png",
 },
 {
-rank: 'king',
-suit: 'hearts',
-cardImage:'images/king-of-hearts.png',
+	rank: 'king',
+	suit: 'hearts',
+	cardImage:"images/king-of-hearts.png",
 },
 {
-rank: 'king',
-suit: 'diamonds',
-cardImage:'images/king-of-diamonds.png',
-}
-	];
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage:"images/king-of-diamonds.png",
+} ];
 
-cardsInPlay = [];
+let cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -34,29 +31,34 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
-var cardId = cardId;
+function flipCard() {
+let cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);
+
 cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
-
-checkForMatch();
-// if (cardsInPlay[0] === cardsInPlay[1]) {
-// alert("You found a match!");
-// } else {
-// alert("Sorry, try again.");
-// };
-};
-
-flipCard(0);
-flipCard(2);
+console.log(cards[cardId].cardImage);
 
 
-// let cardOne = cards[0];
-// cardsInPlay.push('cardOne');
-// console.log("User flipped " + cardOne);
+this.setAttribute('src', cards[cardId].cardImage);
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+	}
+}
 
-// let cardTwo = cards[2];
-// cardsInPlay.push('cardTwo');
-// console.log("User flipped " + cardTwo);
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+
+    document.getElementById('game-board').appendChild(cardElement);
+    //appends cardElement to the game board *id of game-board*
+    cardElement.addEventListener('click', flipCard);
+    //completes game card stored in cardElement
+    
+	}
+}
+
+createBoard();
+//creates board and starts the game
